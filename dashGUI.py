c5e9f894959565023b01424dbaca744ddade544b
tylerdvanl <49462625+tylerdvanl@users.noreply.github.com>
2022-11-26 21:21:56 -0800

    Radial Chart From Data
    Radial Chart reflects data pulled from the CSV file

--- dashGUI.py
+++ dashGUI.py
@@ -1,24 +1,37 @@
 from dash import Dash, html, dcc, Input, Output
 import plotly.express as px 
-import pandas as pd 
+import pandas as pd
+import csv
 
 #n
 #runs on http://127.0.0.1:8050/
 
 app = Dash(__name__)
+#Grab data from CSV file.
+with open('Odds - Sheet1.csv') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    rows = list(csv_reader)
 
+#This is hacky, but i dont know why the radial chart breaks if I do it the easier, more direct way.
+dataValues = [float(rows[1][5]), float(rows[1][6]), float(rows[1][7]), float(rows[1][9])]
+#print(dataValues)
 
-
-
-df = pd.DataFrame(dict(
-        r = [3,3, 3, 4, 5], 
-        theta = ['DataPoint1', 'DataPoint2', 'DataPoint3', 'DataPoint4', 'DataPoint5']
+#Selected columns are F, G, H, and J
+#Defaulting to first row until we decide how to change what team is being viewed
+df1 = pd.DataFrame.from_dict(dict(
+        value = dataValues,
+        description = ['Percent 2-Point Field Goals Attempted', 'Percent 3-Point Field Goals Attempted',
+                              'Percent 2-Point Field Goals Scored', 'Percent 3-Point Field Goals Scored']
         ))
-rad = px.line_polar(df, r= 'r', theta = 'theta', line_close = True)
+#print(df1)
+rad1 = px.line_polar(df1, r= 'value',
+                     theta = 'description',
+                     line_close = True,
+                     range_r = [0, 100])
+#rad1.show()
 
-
-df = px.data.stocks()
-hist = px.histogram(df, x = "date", nbins = 23) #23 weeks in nba season
+df2 = px.data.stocks()
+hist = px.histogram(df2, x = "date", nbins = 23) #23 weeks in nba season
 
 
 app.layout = html.Div(
@@ -31,10 +44,10 @@ app.layout = html.Div(
             id = 'Wins/Loss Histogram',
             figure = hist
         ),
-        html.Div(children = 'Radial Chart of Normalized Data'), 
+        html.Div(children = 'Radial Chart of Normalized Data'),
         dcc.Graph(
-            id ='Radial Visualization', 
-            figure= rad
+            id ='Radial Visualization',
+            figure= rad1
         )
 ])
 
