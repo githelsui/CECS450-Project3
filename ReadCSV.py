eea88aa0445ca81c2e51e6193b06a81589673e2f
Juancy15 <toto1015.ja@gmail.com>
2022-11-27 15:32:37 -0800

    dashGUI Update v1
    - Reorder of Dash GUI Graphs
    - New CSV
    - Update to ReadCSV (filters unnecessary values)

--- ReadCSV.py
+++ ReadCSV.py
@@ -1,16 +1,27 @@
 import csv
 
-# Key to search for (implemented for testing and making sure filtering will work down the line)
-key = input("Enter key you'd like to search for: ")
-# Create a 2D Array by reading csv data
-data = list(csv.reader(open("NBA_Team_Stats.csv")))
 
-# Look through 2D Array and return rows which match term being looked for (In our case, most likely dates)
-for i in range(len(data)):
-  for j in range(len(data[i])):
-      if data[i][j] == key:
-        print(data[i])
+# def teamDataForYears(years):
+#     # Key to search for (implemented for testing and making sure filtering will work down the line)
+#     key = input("Enter key you'd like to search for: ")
+#     # Create a 2D Array by reading csv data
+#     data = list(csv.reader(open("Sports Reference NBA Data (2021 - 2022) Season.xlsx - sportsref_download.xls.csv")))
+#
+#     # Look through 2D Array and return rows which match term being looked for (In our case, most likely dates)
+#     for i in range(len(data)):
+#         for j in range(len(data[i])):
+#             if data[i][j] == key:
+#                 print(data[i])
 
 
-
-
+def teamWinsDataFrame():
+    data = list(csv.reader(open("Sports Reference NBA Data (2021 - 2022) Season.xlsx - sportsref_download.xls.csv")))
+    res = []
+    teamNames = []
+    wins = []
+    for i in range(len(data)):
+        if data[i][1] != "Team" and data[i][1] != "League Averages":
+            teamNames.append(data[i][1])
+            wins.append(data[i][25])
+    res = [teamNames, wins]
+    return res
