7dee4b865330932e1b49280c8ee1e2b11cd63865
Nikki Benitez <39257026+nikkivbenz@users.noreply.github.com>
2022-11-27 16:01:57 -0800

    Update dashGUI.py
    Added the two radial chart dropdowns, the implied probability

--- dashGUI.py
+++ dashGUI.py
@@ -27,7 +27,7 @@ import csv
 
 app = Dash(__name__)
 #Grab data from CSV file.
-with open('Sports Reference NBA Data (2021 - 2022) Season.xlsx - sportsref_download.xls.csv') as csv_file:
+with open('Sports_Reference_NBA_Data_2021_-_2022_Season.xlsx_-_sportsref_download.xls.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
     rows = list(csv_reader)
 
@@ -66,26 +66,97 @@ hist = px.histogram(df2, x = "date", nbins = 23) #23 weeks in nba season
 app.layout = html.Div(
     children=[
         html.H1(children='Sports Betting'),
-        dcc.Dropdown(['OPT 1'], 'OPT 1', id='Test-Dropdown'),
         html.Div(id='td-output'),
         html.Div(children = 'All Teams Wins/Losses'),
         dcc.Graph(
             id = 'Wins/Loss Histogram',
             figure = hist
         ),
-        html.Div(children = 'Radial Chart of Normalized Data'),
-        dcc.Graph(
-            id = 'Radial Visualization',
-            figure = rad1
-        )
+        html.Div(children = ['Implied Probability\n\n', dcc.Input(
+            id='ImpliedProb', 
+            type = 'number',
+            value = 100
+            ), 
+            html.Table([
+                html.Tr([html.Td('Win %: '), html.Td(id='imp')]),
+            ]),
+        ]),
+        html.Div(children = [html.Div(children = 'TEAM 1 VS TEAM 2'),
+            dcc.Dropdown(['Atlanta Hawks', 'Boston Celtics', 'Brooklyn Nets', 'Charlotte Hornets', 'Chicago Bulls', 'Cleveland Cavaliers', 'Dallas Mavericks', 'Denver Nuggets', 'Detroit Pistons', 'Golden State Warriors', 'Houston Rockets', 'Indiana Pacers', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Memphis Grizzlies', 'Miami Heat', 'Milwaukee Bucks', 'Minnesota Timberwolves', 'New Orleans Pelicans', 'New York Knicks', 'Oklahoma City Thunder','Orlando Magic', 'Philadelphia 76ers', 'Phoenix Suns', 'Portland Trail Blazers', 'Sacramento Kings', 'San Antonio Spurs', 'Toronto Raptors', 'Utah Jazz', 'Washington Wizards'], 'Atlanta Hawks', id='drop1', style=dict(width='50%')), 
+            dcc.Dropdown(['Atlanta Hawks', 'Boston Celtics', 'Brooklyn Nets', 'Charlotte Hornets', 'Chicago Bulls', 'Cleveland Cavaliers', 'Dallas Mavericks', 'Denver Nuggets', 'Detroit Pistons', 'Golden State Warriors', 'Houston Rockets', 'Indiana Pacers', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Memphis Grizzlies', 'Miami Heat', 'Milwaukee Bucks', 'Minnesota Timberwolves', 'New Orleans Pelicans', 'New York Knicks', 'Oklahoma City Thunder','Orlando Magic', 'Philadelphia 76ers', 'Phoenix Suns', 'Portland Trail Blazers', 'Sacramento Kings', 'San Antonio Spurs', 'Toronto Raptors', 'Utah Jazz', 'Washington Wizards'], 'Atlanta Hawks', id='drop2', style=dict(width='50%')) 
+
+        ]),
+        html.Div(children = [html.Div(children = "Radial Chart of Normalized Data"), 
+            dcc.Graph(id = 'rad1', figure = rad1, style={'display': 'inline-block'}), 
+                      
+            dcc.Graph(id = 'rad2', figure = rad2, style={'display': 'inline-block'})
+        ])
     ])
 
+
+#this callback allows users to choose the teams they want to see in the radial chart
+#the Input elements are parameters to the function immediately below
+#the Output elements are [rad1, rad2] list that the function returns
+#automatically get connected to eachother
 @app.callback(
-    Output('td-output', 'children'),
-    Input('Test-Dropdown', 'value')
+    Output('rad1', 'figure'),
+    Output('rad2', 'figure'),
+    Input('drop1', 'value'),
+    Input('drop2', 'value')
 )
-def update_output(value):
-    return f'Displaying {value}'
+def update_graph(drop1, drop2):
+    allTeams = ['Atlanta Hawks', 'Boston Celtics', 'Brooklyn Nets', 'Charlotte Hornets', 'Chicago Bulls', 'Cleveland Cavaliers', 'Dallas Mavericks', 'Denver Nuggets', 'Detroit Pistons', 'Golden State Warriors', 'Houston Rockets', 'Indiana Pacers', 'Los Angeles Clippers', 'Los Angeles Lakers', 'Memphis Grizzlies', 'Miami Heat', 'Milwaukee Bucks', 'Minnesota Timberwolves', 'New Orleans Pelicans', 'New York Knicks', 'Oklahoma City Thunder','Orlando Magic', 'Philadelphia 76ers', 'Phoenix Suns', 'Portland Trail Blazers', 'Sacramento Kings', 'San Antonio Spurs', 'Toronto Raptors', 'Utah Jazz', 'Washington Wizards']
+    rowNumber1 = allTeams.index(drop1) + 1
+    rowNumber2 = allTeams.index(drop2) + 1
+
+    #gets the data values from the rows list created when the csv file was imported
+    dataValues1 = [float(rows[rowNumber1][24]), float(rows[rowNumber1][19]), float(rows[rowNumber1][20]), float(rows[rowNumber1][21])]
+    dataValues2 = [float(rows[rowNumber2][24]), float(rows[rowNumber2][19]), float(rows[rowNumber2][20]), float(rows[rowNumber2][21])]
+
+    #normalization would go here
+    descriptionNames = ['Points Per Game', 'Assists Per Game', 'Steals Per Game', 'Blocks Per Game']
+
+    df1 = pd.DataFrame.from_dict(dict(
+        value = dataValues1,
+        description = descriptionNames
+        ))
+
+    df2 = pd.DataFrame.from_dict(dict(
+        value = dataValues2,
+        description = descriptionNames
+        ))
+
+
+    rad1 = px.line_polar(df1, r= 'value',
+                     theta = 'description',
+                     line_close = True,
+                     range_r = [0, 150])
+
+    rad2 = px.line_polar(df2, r= 'value',
+                     theta = 'description',
+                     line_close = True,
+                     range_r = [0, 150])
+
+    return [rad1, rad2]
+
+#implied probability callback
+#allows users to enter a value and get the odds of them winning (as percentages) 
+@app.callback(
+    Output('imp', 'children'),
+    Input('ImpliedProb', 'value')
+)
+
+def updateAmericanOdds(imp):
+    percentage = 0
+    if imp != None: 
+        if (imp >= 0): 
+            percentage = (100 / (imp + 100)) * 100
+        else: 
+            percentage = ((-1*(imp)) / (-1*(imp) + 100)) * 100 
+        asStr = str(percentage)+ " %"
+        return asStr
+    return ""
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
